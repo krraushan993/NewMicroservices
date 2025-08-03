d1e5b8e76e87c5af91eba92331fd1427db9ddca2
krraushan993 <rk733564@gmail.com>
2025-08-03 15:43:51 +0530

    Created Dto class

---
+++ accounts/src/main/java/com/rktech/accounts/dto/AccountsDto.java
@@ -0,0 +1,14 @@
+package com.rktech.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+	
+	private Long AccountNumber;
+	
+	private String accountType;
+	
+	private String branchAddress;
+
+}
---
+++ accounts/src/main/java/com/rktech/accounts/dto/CustomerDto.java
@@ -0,0 +1,5 @@
+package com.rktech.accounts.dto;
+
+public class CustomerDto {
+
+}
