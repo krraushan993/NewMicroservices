8685971a3a686996bacdfd6a65bc85a5386b1b85
krraushan993 <rk733564@gmail.com>
2025-08-17 00:14:09 +0530

    Mistake in post method

--- accounts/src/main/java/com/rktech/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/rktech/accounts/controller/AccountController.java
@@ -3,7 +3,7 @@ package com.rktech.accounts.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rktech.accounts.constants.AccountsConstants;
 import com.rktech.accounts.dto.CustomerDto;
 import com.rktech.accounts.dto.ResponseDto;
+import com.rktech.accounts.service.IAccountService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
+@AllArgsConstructor
 public class AccountController {
 
+	private final IAccountService accountService;
+
+	@PostMapping("/create")
 	public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
 
+		accountService.createAccount(customerDto);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
 	}
